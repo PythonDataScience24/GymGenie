d4470039c1c128d5a5cbe6f53b52c34eb53ffe27
ddd42-star <dario.bassi111@gmail.com>
2024-04-29 14:29:15 +0200

    Added some logic

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     while program:       
         # choose a command
         try:
-            option = input("Choose:\n 1.Statistics\n 2.Add Workouts\n 3.Add Goal\n 4.Visualize Trends\n")
+            option = input("Choose an option between 1 and 5:\n 1.Statistics\n 2.Add Workouts\n 3.Add Goal\n 4.Visualize Trends\n 5.Exit\n")
 
             if(int(option) == 1):
                 print("A")
@@ -22,9 +22,12 @@ if __name__ == "__main__":
             elif(int(option) == 4):
                 print("D")
                 # insert code from kristin&Laura
-            else:
+            elif(int(option) == 5):
                 program = False
                 sys.exit("GymGenie has been terminated")
+            else:
+                print("Please select a value between 1 to 5!")
         except ValueError:
             print("Wrong format. Please use only integers!")
-        
+
+# write all in a main function and then call it in the _name__==__main__        
