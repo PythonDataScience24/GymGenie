0ae4abb3032448398c68d424c3ebcd6e24e9feaf
Kristin Watnedal Olsen <kristin@watnedal.com>
2024-04-30 17:20:12 +0200

    Added sub_classname method

--- src/workout.py
+++ src/workout.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class Workout(ABC):
     def __init__(self, calories, date, distance, duration, rating):
@@ -9,40 +9,63 @@ class Workout(ABC):
         self.rating = rating
 
 
-    #@abstractmethod
-    #def role(self):
-    #    pass
+    @abstractmethod
+    def subclass_name(self):
+        pass
+
 
 class Running(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Cycling(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Strength(Workout):
     def __init__(self, calories, date, duration, rating):
-        super.__init__(self, calories, date, None, duration, rating)
+        super().__init__(calories, date, None, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Swimming(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Skiing(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Walking(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Climbing(Workout):
     def __init__(self, calories, date,  duration, rating):
-        super.__init__(self, calories, date, None, duration, rating)
+        super().__init__(calories, date, None, duration, rating)
+
+    def subclass_name(self):
+        return self.__class__.__name__
 
 class Other(Workout):
     def __init__(self, calories, date, distance, duration, rating):
-        super.__init__(self, calories, date, distance, duration, rating)
+        super().__init__(calories, date, distance, duration, rating)
 
-    
+    def subclass_name(self):
+        return self.__class__.__name__
