d0bb2847394581b4cbdc06d797cfc939f7a94a52
Kristin Watnedal Olsen <kristin@watnedal.com>
2024-04-30 22:08:03 +0200

    Added documentation and some example usages

--- src/duration.py
+++ src/duration.py
@@ -1,19 +1,51 @@
 class Duration:
+    """
+    Represents the duration of a workout.
+
+    Attributes:
+        hours (float): How many hours the workout lasted for.
+        min (float): How many minutes the workout lasted for.
+    """
     def __init__(self, hours=0, minutes=0):
-        self.minutes = minutes + 60*hours
+        self.minutes = minutes + 60*hours # Save duration as the total minutes spent.
 
     def __str__(self):
-        return self.short_str()
-        # return self.long_str()    
+        return self.short_str()   
 
-    def short_str(self):     
+    def short_str(self):  
+            """
+            Return duration in short string format. Example: "1h30".
+            """   
             return f"{self.get_hours()}h{self.get_minutes()}"
     
-    def long_str(self):     
+    def long_str(self):
+        """
+        Return duration in long string format. Example: "1 hours, 30 minutes".
+        """   
         return f"{self.get_hours()} hours, {self.get_minutes()} minutes"
 
     def get_hours(self):
+        """
+        Get the number of full hours of the duration.
+        """
         return self.minutes // 60
     
     def get_minutes(self):
+        """
+        Get the number of residual minutes of the duration after the hours has been accounted for.
+        """
         return self.minutes % 60
+    
+# Example usage
+if __name__ == '__main__':
+        my_duration = Duration(hours = 1, minutes = 30)
+
+        # Print duration as short and long string
+        print(my_duration.short_str())
+        print(my_duration.long_str())
+
+        # Get hours and minutes seperately
+        print(my_duration.get_hours())
+        print(my_duration.get_minutes())
+
+
--- src/workout.py
+++ src/workout.py
@@ -1,6 +1,16 @@
 from abc import ABC, abstractmethod
 
 class Workout(ABC):
+    """
+    Abstract base class for a workout.
+
+    Attributes:
+        calories (Calories): Calories burnt during workout.
+        date (Date): Date of the workout.
+        distance (Distance): Distance covered during the workout.
+        duration (Duration): The duration of the workout.
+        rating (Rating): Rating of how the workout felt.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         self.calories = calories
         self.date = date
@@ -8,13 +18,18 @@ class Workout(ABC):
         self.duration = duration
         self.rating = rating
 
-
     @abstractmethod
     def subclass_name(self):
+        """
+        Returns the name of the subclass (type of workout).
+        """
         pass
 
 
 class Running(Workout):
+    """
+    A running workout. See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
@@ -22,6 +37,9 @@ class Running(Workout):
         return self.__class__.__name__
 
 class Cycling(Workout):
+    """
+    A cycling workout. See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
@@ -29,6 +47,10 @@ class Cycling(Workout):
         return self.__class__.__name__
 
 class Strength(Workout):
+    """
+    A strength workout. See Workout for attributes.
+    Distance is set to None for this type of Workout.
+    """
     def __init__(self, calories, date, duration, rating):
         super().__init__(calories, date, None, duration, rating)
 
@@ -36,6 +58,9 @@ class Strength(Workout):
         return self.__class__.__name__
 
 class Swimming(Workout):
+    """
+    A swimming workout. See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
@@ -43,6 +68,9 @@ class Swimming(Workout):
         return self.__class__.__name__
 
 class Skiing(Workout):
+    """
+    A skiing workout. See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
@@ -50,6 +78,9 @@ class Skiing(Workout):
         return self.__class__.__name__
 
 class Walking(Workout):
+    """
+    A walking workout. See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
@@ -57,6 +88,10 @@ class Walking(Workout):
         return self.__class__.__name__
 
 class Climbing(Workout):
+    """
+    A climbing workout. See Workout for attributes.
+    Distance is set to None in this type of Workout.
+    """
     def __init__(self, calories, date,  duration, rating):
         super().__init__(calories, date, None, duration, rating)
 
@@ -64,8 +99,13 @@ class Climbing(Workout):
         return self.__class__.__name__
 
 class Other(Workout):
+    """
+    A workout that is not running, cycling, strenght, swimming, skiing, walking and climbing. 
+    See Workout for attributes.
+    """
     def __init__(self, calories, date, distance, duration, rating):
         super().__init__(calories, date, distance, duration, rating)
 
     def subclass_name(self):
         return self.__class__.__name__
+    
