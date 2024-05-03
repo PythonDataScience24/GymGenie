3c775cb59fa5531216ebf938f70f113ddd98a3dd
ddd42-star <dario.bassi111@gmail.com>
2024-05-04 00:27:11 +0200

    Added some details for the barplot

--- src/goal_summary.py
+++ src/goal_summary.py
@@ -58,8 +58,9 @@ class GoalSummary:
 
         # filter the logWorkout dataframe, containing only all the activities with the same exercise
         # and according to the timeframe
+        print(self.logWorkoutDataframe)
         filtered_workout_dataframe = self.logWorkoutDataframe[(self.logWorkoutDataframe['activity'] == exercise) & 
-                                                               (self.logWorkoutDataframe['date'] >= start_time) |
+                                                               (self.logWorkoutDataframe['date'] >= start_time) &
                                                                (self.logWorkoutDataframe['date'] <= end_time)]
         print(filtered_workout_dataframe)
         # convert all distance in standard km? not necessary for the moment
@@ -67,17 +68,21 @@ class GoalSummary:
         # add an horizontal line to shows where the goal is
         match unit_value:
             case "kcal":
-                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['calories'])
+                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['calories'], edgecolor='gray')
                 ylabel = "Calories"
             case "km":
-                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['distance'])
+                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['distance'],edgecolor='gray')
                 ylabel = "Distance"
             case "min":
-                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['duration'])
+                plt.bar(filtered_workout_dataframe['date'], filtered_workout_dataframe['duration'],edgecolor='gray')
                 ylabel = "Duration"
         # add goal line in the barplot
-        #plt.hlines(value_goal,xmin=0, xmax=len(filtered_workout_dataframe['date']), linestyles='--', lw=2,colors='black')
-        plt.ylabel(ylabel)
+        plt.axhline(value_goal, linestyle='--', lw=1.2,color='black', label="Goal", zorder=-1.5)
+        plt.ylabel(ylabel, fontsize=12)
+        plt.xticks(rotation=-25, fontsize=8)
+        #plt.xlabel('Time', fontsize=12)
+        plt.legend(frameon=False)
+        plt.title(exercise)
         plt.show()
 
         return True
@@ -92,7 +97,7 @@ if __name__=="__main__":
     logWork = pd.DataFrame({'activity': ['Running','Cycling','Running','Swimming','Running'], 'date': [date(2024,4,20),date(2024,4,21),date(2024,4,22),date(2024,4,23),date(2024,4,24)] , 'duration' : [40,120,30,20,20],
                                     'distance' : [9,40,6,2,8], 'calories' : [200,500,250,400,300], 'rating' : [8,9,4,5,6]})
 
-    goalFrame = pd.DataFrame({"value":[300,100], "unit":['min','km'], "time_scale":[7,7], "start_date":[date(2024,4,20),date(2024,5,20)], 
+    goalFrame = pd.DataFrame({"value":[35,100], "unit":['min','km'], "time_scale":[7,7], "start_date":[date(2024,4,20),date(2024,5,20)], 
                                 "end_date":[date(2024,4,24), date(2024,5,24)], "exercise":['Running','Cycling']})
 
     summary = GoalSummary(logWork,goalFrame)
