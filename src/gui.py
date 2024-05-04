9bc87b7625004d406893f81a6ef53739025c6c19
Kristin Watnedal Olsen <kristin@watnedal.com>
2024-05-04 18:35:00 +0200

    Compacted code and initilized functions for all buttons on start page

--- src/gui.py
+++ src/gui.py
@@ -11,10 +11,25 @@ dark_blue = "#005F73"
 blue = "#357F93"
 light_blue = "#5d99a9"
 
-# lage create_button funksjon(tar mye parametere som input)
 # liste med text
 # liste med funksjoner den skal bruke - lambda
 
+def create_button(frame, command, text, font, width, height):
+    button = tk.Button(
+        frame,
+        command=command,
+        text=text,
+        font=font,
+        background=dark_blue,
+        foreground=white,
+        activebackground=light_blue,
+        activeforeground=white,
+        width=width,
+        height=height, 
+        border=0,
+        cursor="hand2"
+    )
+    return button
 
 def display_start_page():
     """
@@ -47,67 +62,16 @@ def display_start_page():
     )
     label.grid(column=0, row=0)
 
-    log_workout_button = tk.Button(
-        main_frame,
-        command=lambda: choose_workout(root),
-        text = "Log a workout",
-        font =("Arial", 12, "bold"),
-        background=dark_blue,
-        foreground=white,
-        activebackground=light_blue,
-        activeforeground=white,
-        width=20,
-        height=2, 
-        border=0,
-        cursor="hand2"
-    )
-    log_workout_button.grid(column=0, row=1)
+    start_page_buttons = [("Log a workout", lambda: choose_workout(root)), 
+                          ("Set a new goal", lambda: set_goal(root)),
+                          ("View goals", lambda: view_goals(root)), 
+                          ("View progress and trends", lambda: view_progress_and_trends(root))]
 
-    log_workout_button = tk.Button(
-        main_frame, 
-        text = "Set a new goal",
-        font =("Arial", 12, "bold"),
-        background=dark_blue,
-        foreground=white,
-        activebackground=light_blue,
-        activeforeground=white,
-        width=20,
-        height=2, 
-        border=0,
-        cursor="hand2"
-    )
-
-    log_workout_button.grid(column=0, row=2)
-
-    log_workout_button = tk.Button(
-        main_frame, 
-        text = "View goals",
-        font =("Arial", 12, "bold"),
-        background=dark_blue,
-        foreground=white,
-        activebackground=light_blue,
-        activeforeground=white,
-        width=20,
-        height=2, 
-        border=0,
-        cursor="hand2"
-    )
-    log_workout_button.grid(column=0, row=3)
-
-    log_workout_button = tk.Button(
-        main_frame, 
-        text = "View progress and trends",
-        font =("Arial", 12, "bold"),
-        background=dark_blue,
-        foreground=white,
-        activebackground=blue,
-        activeforeground=white,
-        width=20,
-        height=2, 
-        border=0,
-        cursor="hand2"
-    )
-    log_workout_button.grid(column=0, row=4)
+    for i, button in enumerate(start_page_buttons):
+        start_page_button = create_button(frame=main_frame, command=button[1],
+                                          text=button[0], font=("Arial", 12, "bold"), 
+                                          width=20, height=2)
+        start_page_button.grid(column=0, row=i+1)
 
     tk.mainloop()
 
@@ -118,24 +82,26 @@ def choose_workout(root):
     choose_frame = tk.Frame(root, bg=blue, pady=40)
     choose_frame.pack(fill=tk.BOTH, expand=True)
 
-def chooseWorkout(start_window):
-    """
-    Allows the user to chose the type of workout they want to log.
-    """
-    start_window.destroy()
-    type_window = tk.Tk()
-    tk.Label(type_window, text="Choose a type of workout").pack()
+def set_goal(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    set_goal_frame = tk.Frame(root, bg=blue, pady=40) # frame can be renamed so it fits best with your code.
+    set_goal_frame.pack(fill=tk.BOTH, expand=True) 
     
-    for type in WORKOUT_TYPES:
-        tk.Button(type_window, text=type, command=lambda: logWorkout(type_window)).pack()
 
+def view_goals(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
-def logWorkout(type_window):
-    """
-    Allows the user to log a type of workout.
-    """
-    type_window.destroy()
-    log_window = tk.Tk()
-    tk.Label(log_window, text="Log a workout").pack()
+    view_goal_frame = tk.Frame(root, bg=blue, pady=40) # same here for renaming
+    view_goal_frame.pack(fill=tk.BOTH, expand=True)
+
+def view_progress_and_trends(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    progress_trend_frame = tk.Frame(root, bg=blue, pady=40)
+    progress_trend_frame.pack(fill=tk.BOTH, expand=True)
 
 display_start_page()
