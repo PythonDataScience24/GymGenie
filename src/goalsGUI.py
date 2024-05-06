19ed84302bfbc3e893d1dfd269d49ea4758f94c0
lfercer <laura.fernandez@students.unibe.ch>
2024-05-06 13:06:50 +0200

    display of duration, distance and calories created

--- src/goalsGUI.py
+++ src/goalsGUI.py
@@ -13,6 +13,8 @@ blue = "#357F93"
 light_blue = "#5d99a9"
 
 
+root = tk.Tk()
+root.geometry("500x400")
 
 def display_set_goal():
     """
@@ -23,10 +25,6 @@ def display_set_goal():
         Calories burned
     
     """
-
-    root = tk.Tk()
-    root.geometry("500x400")
-
     #Create the main frame
     main_frame = tk.Frame(root, bg=blue, pady=40)
     main_frame.pack(fill=tk.BOTH, expand=True)
@@ -50,9 +48,11 @@ def display_set_goal():
 
     #Create goal buttons based on the type of objective
     GOALS_TYPES = ['Distance', 'Duration', 'Calories burned']
+    commands = [display_distance, display_duration, display_calories]
     for i, goal_type in enumerate(GOALS_TYPES):
         btn = tk.Button( main_frame, 
-            text = goal_type,
+            command= commands[i],
+            text = goal_type, 
             font =("Arial", 12, "bold"),
             background=white,
             foreground=dark_blue,
@@ -63,11 +63,143 @@ def display_set_goal():
             border=0,
             cursor="hand2",
             borderwidth=1, #Add border width
-            relief="solid") #Add relief style # I've read that with Tkinter you can not change the color o fthe buttons :(
+            relief="solid") #Add relief style 
 
         
         btn.grid(column=0, row=1+i) 
 
 
-    root.mainloop()
 
+
+
+def display_duration():
+
+    # Remove all widgets from the root window
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    #Create the main frame
+    main_frame = tk.Frame(root, bg=blue, pady=40)
+    main_frame.pack(fill=tk.BOTH, expand=True)
+
+    #Configure columns and rows
+    main_frame.columnconfigure(0, weight=1)
+    main_frame.columnconfigure(1, weight=1)
+    main_frame.columnconfigure(2, weight=1)
+    main_frame.rowconfigure(0, weight=1)
+    main_frame.rowconfigure(1, weight=1)
+    main_frame.rowconfigure(2, weight=1)
+    main_frame.rowconfigure(3, weight=1)
+    main_frame.rowconfigure(4, weight=1)
+
+    #create duration label
+    duration_label = tk.Label(main_frame, text="Duration:",
+                        font =("Arial", 16, "bold"),
+            background=blue,
+            foreground=white,
+            width=20,
+            height = 1)
+    duration_label.grid(column=0, row=0)
+
+    #create hours and minutes entry
+    e_hours = tk.Entry(main_frame)
+    e_hours.insert(tk.END, "h")
+    e_min = tk.Entry(main_frame)
+    e_min.insert(tk.END, "min")
+    e_hours.grid(column=1, row=0)
+    e_min.grid(column=2, row=0)
+
+
+def display_distance():
+    # Remove all widgets from the root window
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    #Create the main frame
+    main_frame = tk.Frame(root, bg=blue, pady=40)
+    main_frame.pack(fill=tk.BOTH, expand=True)
+
+    #Configure columns and rows
+    main_frame.columnconfigure(0, weight=1)
+    main_frame.columnconfigure(1, weight=1)
+    main_frame.columnconfigure(2, weight=1)
+    main_frame.columnconfigure(3, weight=1)
+    main_frame.rowconfigure(0, weight=1)
+    main_frame.rowconfigure(1, weight=1)
+    main_frame.rowconfigure(2, weight=1)
+    main_frame.rowconfigure(3, weight=1)
+    main_frame.rowconfigure(4, weight=1)
+
+    #create distance label
+    distance_label = tk.Label(main_frame, text="Distance:",
+                        font =("Arial", 16, "bold"),
+            background=blue,
+            foreground=white,
+            width=20,
+            height = 1)
+    distance_label.grid(column=0, row=0)
+
+    #create km, m, miles  entries
+    e_km = tk.Entry(main_frame)
+    e_km.insert(tk.END, "km")
+    e_m = tk.Entry(main_frame)
+    e_m.insert(tk.END, "m")
+    e_miles = tk.Entry(main_frame)
+    e_miles.insert(tk.END, "miles")
+    e_km.grid(column=1, row=0)
+    e_m.grid(column=2, row=0)
+    e_miles.grid(column=3, row=0)
+
+
+
+
+def display_calories():
+
+    # Remove all widgets from the root window
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    #Create the main frame
+    main_frame = tk.Frame(root, bg=blue, pady=40)
+    main_frame.pack(fill=tk.BOTH, expand=True)
+
+    #Configure columns and rows
+    main_frame.columnconfigure(0, weight=1)
+    main_frame.columnconfigure(1, weight=1)
+    main_frame.columnconfigure(2, weight=1)
+    main_frame.rowconfigure(0, weight=1)
+    main_frame.rowconfigure(1, weight=1)
+    main_frame.rowconfigure(2, weight=1)
+    main_frame.rowconfigure(3, weight=1)
+    main_frame.rowconfigure(4, weight=1)
+
+    #create duration label
+    calories_label = tk.Label(main_frame, text="Calories burned:",
+                        font =("Arial", 16, "bold"),
+            background=blue,
+            foreground=white,
+            width=20,
+            height = 1)
+    calories_label.grid(column=0, row=0)
+
+    #create kcal and kJ entry
+    e_kcal = tk.Entry(main_frame)
+    e_kcal.insert(tk.END, "kcal")
+    e_kJ = tk.Entry(main_frame)
+    e_kJ.insert(tk.END, "kJ")
+    e_kcal.grid(column=1, row=0)
+    e_kJ.grid(column=2, row=0)
+
+
+
+
+
+
+
+
+
+
+
+display_set_goal()
+#display_duration()
+root.mainloop()
