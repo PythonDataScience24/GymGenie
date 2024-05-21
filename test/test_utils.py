00c7ee0290356eb76f15cf645fc7c351f89a22f5
Kristin Watnedal Olsen <kristin@watnedal.com>
2024-05-21 15:06:31 +0200

    Added tests for Duration and text about it in testing.md

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,6 +5,7 @@ import os
 #we need to add the src folder to sys.path before being able to import the module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from utils import Distance
+from utils import Duration
 
 class TestDistance(unittest.TestCase):
 
@@ -29,3 +30,46 @@ class TestDistance(unittest.TestCase):
         self.assertAlmostEqual(self.distance_m.distance_value, 0.062, "should be 0.062")
         self.distance_miles.distance_convert("miles", "m")
         self.assertAlmostEqual(self.distance_miles.distance_value, 8046.7, "should be 8046.7")
+
+
+class TestDuration(unittest.TestCase):
+
+    def test_init(self):
+        duration1 = Duration(1,30)
+        self.assertAlmostEqual(duration1.minutes, 90, "Should be 90.")
+        duration2 = Duration(2)
+        self.assertAlmostEqual(duration2.minutes, 120, "Should be 120.")
+        duration3 = Duration(minutes=150)
+        self.assertAlmostEqual(duration3.minutes, 150, "Should be 150.")
+        duration4 = Duration()
+        self.assertAlmostEqual(duration4.minutes, 0, "Should be 0.")
+
+    def test_short_str(self):
+        duration1 = Duration(1,30)
+        self.assertEqual(duration1.minutes, "1h30", "Should be 1h30.")
+        duration2 = Duration(2)
+        self.assertEqual(duration2.minutes, "2h0", "Should be 2h0.")
+        duration3 = Duration(minutes=150)
+        self.assertEqual(duration3.minutes, "2h30", "Should be 2h30.")
+        duration4 = Duration()
+        self.assertEqual(duration4.minutes, "0h0", "Should be 0h0.")
+
+    def test_get_hours(self):
+        duration1 = Duration(1,30)
+        self.assertAlmostEqual(duration1.get_hours(), 1, "Should be 1.")
+        duration2 = Duration(2)
+        self.assertAlmostEqual(duration2.get_hours(), 2, "Should be 2.")
+        duration3 = Duration(minutes=150)
+        self.assertAlmostEqual(duration3.get_hours(), 2, "Should be 2.")
+        duration4 = Duration()
+        self.assertAlmostEqual(duration4.get_hours(), 0, "Should be 0.")
+
+    def test_get_minutes(self):
+        duration1 = Duration(1,30)
+        self.assertAlmostEqual(duration1.get_minutes(), 30, "Should be 30.")
+        duration2 = Duration(2)
+        self.assertAlmostEqual(duration2.get_minutes(), 0, "Should be 0.")
+        duration3 = Duration(minutes=150)
+        self.assertAlmostEqual(duration3.get_minutes(), 30, "Should be 30.")
+        duration4 = Duration()
+        self.assertAlmostEqual(duration4.get_minutes(), 0, "Should be 0.")
--- testing.md
+++ testing.md
@@ -5,6 +5,8 @@ For the unit testing, we chose some functions, to start at a small scale.
 # Test the function Distance.distance_convert()
 To test this function, we used a glass-box approach, where we looked at all the possible paths we can take through this function, with the different conversions between combinations of km, m and miles. However, we might miss some boundary cases with this approach, for example when the input to the function is of an unexpected format or type
 
+# Testing Duration functions
+Several functions found in the class Durations was tested: init, short_str, get_hours, get_minutes. We expect that the init method will be able to store hours and minutes as only total number of minutes, and that the get_hours and get_minutes methods will be able to extract the number of full hours and the remaining minutes. Since the short_str method depends on get_hours and get_minutes methods that again depend on the convertion in the init_method, we decided to again do a glass-box approach where we follow the possible paths of the conversions from init to get_hours/get_minutes and to short_str. Therefore, we tested possible paths in 4 cases with different input situations from the user: one where both hours and minutes is provided as input, one where only hours is provided, one where only minutes is provided and one with no input.
 
 # Other ideas for functions to test:
 Distance.distance_unit_setting(), which can raise an exception
